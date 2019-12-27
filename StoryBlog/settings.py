9a075c993af7f5516f0e7f501867263fbdba6397
Christian Rodriguez <Christiandecolombia>
2019-12-27 00:05:42 -0800

    added accounts page

--- StoryBlog/settings.py
+++ StoryBlog/settings.py
@@ -57,6 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': ['apps/main/templates',
+                'apps/accounts/templates',
                 'StoryBlog/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
