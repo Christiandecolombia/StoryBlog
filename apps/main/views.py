a12f6e8d49b3e5e9f0f2a8b04fc1233ca5ac63bd
Christian Rodriguez <Christiandecolombia>
2020-01-02 17:57:53 -0800

    working on urls

--- apps/main/views.py
+++ apps/main/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse
 from django.contrib import messages
 
 def index(request):
-    return render(request, 'main/index.html', {})
+    print("*********************views/index")
+    return render(request, 'main/index.html')
 
 def frontpage(request):
+    print("*********************views/frontpage")
     return render(request, 'main/frontpage.html')
+    
